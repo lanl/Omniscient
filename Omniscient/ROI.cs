90a56ea19a525885573fd29a2225c0e845ca73f7
Marc Lavi Ruch <ruch@lanl.gov>
2017-11-08 13:02:53 -0700

    Added inital ROI and Spectrum classes

---
+++ Omniscient/ROI.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omniscient
+{
+    class ROI
+    {
+        public enum BG_TYPE { NONE, FLAT, LINEAR}
+
+        double roiStart;    // keV
+        double roiEnd;      // keV
+
+        double bg1Start;
+        double bg1End;
+        double bg2Start;
+        double bg2End;
+
+        BG_TYPE bgType;
+
+        public ROI()
+        {
+            roiStart = 0;
+            roiEnd = 0;
+            bg1Start = 0;
+            bg1End = 0;
+            bg2Start = 0;
+            bg2End = 0;
+            bgType = BG_TYPE.NONE;
+        }
+
+        public double GetROICounts(Spectrum spec)
+        {
+            int[] counts = spec.GetCounts();
+            double[] bins = spec.GetBins();
+            double totalCounts = 0;
+            int roiBins = 0;
+            double bg1Counts = 0;
+            int bg1Bins = 0;
+            double bg2Counts = 0;
+            int bg2Bins = 0;
+            for (int i=0; i<bins.Length;i++)
+            {
+                if (bins[i] >= roiStart && bins[i] <= roiEnd)
+                {
+                    totalCounts += counts[i];
+                    roiBins++;
+                }
+                if (bins[i] >= bg1Start && bins[i] <= bg1End)
+                {
+                    bg1Counts += counts[i];
+                    bg1Bins++;
+                }
+                if (bins[i] >= bg2Start && bins[i] <= bg2End)
+                {
+                    bg2Counts += counts[i];
+                    bg2Bins++;
+                }
+            }
+            switch (bgType)
+            {
+                case BG_TYPE.NONE:
+                    return totalCounts;
+                case BG_TYPE.FLAT:
+                    return totalCounts - (bg1Counts * roiBins / bg1Bins);
+                case BG_TYPE.LINEAR:
+                    return totalCounts - roiBins * ((bg1Counts / bg1Bins) + (bg2Counts / bg2Bins)) / 2;
+            }
+
+            return 0;
+        }
+
+        public double GetROIStart() { return roiStart; }
+        public double GetROIEnd() { return roiEnd; }
+        public double GetBG1Start() { return bg1Start; }
+        public double GetBG1End() { return bg1End; }
+        public double GetBG2Start() { return bg2Start; }
+        public double GetBG2End() { return bg2End; }
+
+        public void SetROIStart(double newStart) { roiStart = newStart; }
+        public void SetROIEnd(double newEnd) { roiEnd = newEnd; }
+        public void SetBG1Start(double newStart) { bg1Start = newStart; }
+        public void SetBG1End(double newEnd) { bg1End = newEnd; }
+        public void SetBG2Start(double newStart) { bg2Start = newStart; }
+        public void SetBG2End(double newEnd) { bg2End = newEnd; }
+    }
+}
---
+++ Omniscient/Spectrum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omniscient
+{
+    class Spectrum
+    {
+        double calibrationZero;
+        double calibrationSlope;
+        int[] counts;
+        
+        public Spectrum()
+        {
+            calibrationZero = 0.0;
+            calibrationSlope = 1.0;
+            counts = new int[0];
+        }
+
+        public Spectrum(double zero, double slope, int[] newCounts)
+        {
+            calibrationZero = zero;
+            calibrationSlope = slope;
+            counts = newCounts;
+        }
+
+        public double[] GetBins()
+        {
+            double[] bins = new double[counts.Length];
+            for (int i = 0; i < bins.Length; i++)
+                bins[i] = calibrationZero + calibrationSlope;
+            return bins;
+        }
+
+        public double GetCalibrationZero() { return calibrationZero; }
+        public double GetCalibrationSlope() { return calibrationSlope; }
+        public int[] GetCounts() { return counts; }
+
+        public void SetCalibrationZero(double zero) { calibrationZero = zero; }
+        public void SetCalibrationSlope(double slope) { calibrationSlope = slope; }
+        public void SetCounts(int[] newCounts) { counts = newCounts; }
+    }
+}
