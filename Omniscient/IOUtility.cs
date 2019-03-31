c274a4162be0f8cd5a419402dfae716df0760b1b
Marc Ruch <ruch@lanl.gov>
2019-03-31 15:20:47 -0600

    Created DateCompact() for compressing List<DateTime>

--- Omniscient/IOUtility.cs
+++ Omniscient/IOUtility.cs
@@ -29,5 +29,122 @@ namespace Omniscient
                 }
             }
         }
+
+        /// <summary>
+        /// Compacts a list of DateTimes into a list of longs (Ticks) using a
+        /// twist on run length encoding: it looks at differences and applies
+        /// RLE to those.
+        /// </summary>
+        /// <param name="times"></param>
+        /// <returns></returns>
+        public static List<long> DateCompact(List<DateTime> times)
+        {
+            int nTimes = times.Count();
+            List<long> output = new List<long>(times.Count());
+            if (nTimes < 1) return output;
+            output.Add(times[0].Ticks);
+            if (nTimes == 1) return output;
+            output.Add(times[1].Ticks);
+            if (nTimes == 2) return output;
+            output.Add(times[2].Ticks);
+
+            int outIndex = 3;
+            long delta1 = output[1] - output[0];
+            long delta2 = output[2] - output[1];
+
+            bool inRun = delta2==delta1;
+            long runCount = 0;
+
+            for (int i=3; i<nTimes; i++)
+            {
+                if (inRun)
+                {
+                    if (times[i].Ticks - times[i - 1].Ticks == delta2)
+                    {
+                        runCount++;
+                        continue;
+                    }
+                    else
+                    {
+                        inRun = false;
+                        output.Add(runCount);
+                        output.Add(times[i].Ticks);
+                        outIndex+=2;
+                        runCount = 0;
+                    }
+                }
+                else
+                {
+                    delta1 = output[outIndex - 2] - output[outIndex - 3];
+                    delta2 = output[outIndex - 1] - output[outIndex - 2];
+                    if (delta1 == delta2)
+                    {
+                        inRun = true;
+                        if (times[i].Ticks - times[i - 1].Ticks == delta2)
+                        {
+                            runCount++;
+                            continue;
+                        }
+                        else
+                        {
+                            inRun = false;
+                            output.Add(runCount);
+                            output.Add(times[i].Ticks);
+                            outIndex += 2;
+                            runCount = 0;
+                        }
+                    }
+                    else
+                    {
+                        output.Add(times[i].Ticks);
+                        outIndex++;
+                    }
+                }
+            }
+            if (inRun) output.Add(runCount);
+
+            return output;
+        }
+
+        /// <summary>
+        /// Reverses DateCompact(List<DateTime> times)
+        /// </summary>
+        /// <param name="ticks"></param>
+        /// <returns></returns>
+        public static List<DateTime> DateUnpack(List<long> ticks)
+        {
+            int nTicks = ticks.Count();
+            List<DateTime> output = new List<DateTime>(nTicks);
+
+            if (nTicks < 1) return output;
+            output.Add(new DateTime(ticks[0]));
+            if (nTicks == 1) return output;
+            output.Add(new DateTime(ticks[1]));
+            if (nTicks == 2) return output;
+            output.Add(new DateTime(ticks[2]));
+
+            long delta1, delta2;
+            long count = 0;
+            for(int t=3; t<nTicks; t++)
+            {
+                delta1 = ticks[t - 2] - ticks[t - 3];
+                delta2 = ticks[t - 1] - ticks[t - 2];
+                if(delta1==delta2)
+                {
+                    count = ticks[t-1];
+                    for(int i=0; i <ticks[t]; i++)
+                    {
+                        count += delta1;
+                        output.Add(new DateTime(count));
+                    }
+                }
+                else
+                {
+                    output.Add(new DateTime(ticks[t]));
+                }
+            }
+
+            return output;
+        }
     }
 }
---
+++ OmniscientTests/CacheTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Omniscient;
+
+namespace OmniscientTests
+{
+    [TestClass]
+    public class CacheTest
+    {
+        [TestMethod]
+        public void DateCompactWorks()
+        {
+            List<DateTime> times = new List<DateTime>()
+            {
+                new DateTime(91),
+                new DateTime(91),
+                new DateTime(91),
+                new DateTime(91),
+                new DateTime(90),
+                new DateTime(91),
+                new DateTime(90),
+                new DateTime(92),
+                new DateTime(93),
+                new DateTime(94),
+                new DateTime(95),
+                new DateTime(96),
+                new DateTime(97),
+                new DateTime(96),
+                new DateTime(95),
+                new DateTime(96),
+            };
+
+            List<long> cTimes = IOUtility.DateCompact(times);
+
+            Assert.AreEqual(cTimes[0], 91);
+            Assert.AreEqual(cTimes[1], 91);
+            Assert.AreEqual(cTimes[2], 91);
+            Assert.AreEqual(cTimes[3], 1);
+            Assert.AreEqual(cTimes[4], 90);
+            Assert.AreEqual(cTimes[5], 91);
+            Assert.AreEqual(cTimes[6], 90);
+            Assert.AreEqual(cTimes[7], 92);
+            Assert.AreEqual(cTimes[8], 93);
+            Assert.AreEqual(cTimes[9], 94);
+            Assert.AreEqual(cTimes[10], 3);
+            Assert.AreEqual(cTimes[11], 96);
+            Assert.AreEqual(cTimes[12], 95);
+            Assert.AreEqual(cTimes[13], 96);
+        }
+
+        [TestMethod]
+        public void DateUnpackWorks()
+        {
+            List<long> ticks = new List<long>()
+            {
+                91,91,91,1,90,91,90,92,93,94,3,96,95,96
+            };
+
+            List<DateTime> times = IOUtility.DateUnpack(ticks);
+
+            Assert.AreEqual(times[0].Ticks, 91);
+            Assert.AreEqual(times[1].Ticks, 91);
+            Assert.AreEqual(times[2].Ticks, 91);
+            Assert.AreEqual(times[3].Ticks, 91);
+            Assert.AreEqual(times[4].Ticks, 90);
+            Assert.AreEqual(times[5].Ticks, 91);
+            Assert.AreEqual(times[6].Ticks, 90);
+            Assert.AreEqual(times[7].Ticks, 92);
+            Assert.AreEqual(times[8].Ticks, 93);
+            Assert.AreEqual(times[9].Ticks, 94);
+            Assert.AreEqual(times[10].Ticks, 95);
+            Assert.AreEqual(times[11].Ticks, 96);
+            Assert.AreEqual(times[12].Ticks, 97);
+            Assert.AreEqual(times[13].Ticks, 96);
+            Assert.AreEqual(times[14].Ticks, 95);
+            Assert.AreEqual(times[15].Ticks, 96);
+        }
+    }
+}
