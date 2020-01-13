92c66e291c7052dd0708a6b50d7675440c1cf4ca
Marc L. Ruch <M.Ruch@iaea.org>
2020-01-13 09:13:35 +0100

    Updated version (0.4.3i)

--- Omniscient/OmniscientCore.cs
+++ Omniscient/OmniscientCore.cs
@@ -24,7 +24,7 @@ namespace Omniscient
         /// <summary>
         /// Version of Omniscient
         /// </summary>
-        public const string VERSION = "0.4.2";
+        public const string VERSION = "0.4.3i";
 
         public event EventHandler ViewChanged;
 
