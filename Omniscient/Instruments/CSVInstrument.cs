8e8eccf869283881ecf24ec6743d045bb68cfd61
Marc L. Ruch <M.Ruch@iaea.org>
2020-05-11 12:04:35 +0200

    Updated copyright

--- Omniscient/Instruments/CSVInstrument.cs
+++ Omniscient/Instruments/CSVInstrument.cs
@@ -1,5 +1,6 @@
 // This software is open source software available under the BSD-3 license.
 // 
+// Copyright (c) 2020, International Atomic Energy Agency (IAEA), IAEA.org
 // Copyright (c) 2018, Triad National Security, LLC
 // All rights reserved.
 // 
@@ -11,7 +12,7 @@
 // 3.       Neither the name of Triad National Security, LLC, Los Alamos National Laboratory, LANL, the U.S. Government, nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
 //  
 // THIS SOFTWARE IS PROVIDED BY TRIAD NATIONAL SECURITY, LLC AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL TRIAD NATIONAL SECURITY, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
