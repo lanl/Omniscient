dddac71885fdbfd8ad3e5cbfa2af52b4726f9385
Marc L. Ruch <ruch@lanl.gov>
2024-08-27 18:33:37 -0600

    Added ChannelRangeStatisticAnalyzerStep

--- Omniscient/Analysis/AnalyzerStep.cs
+++ Omniscient/Analysis/AnalyzerStep.cs
@@ -17,9 +17,10 @@ namespace Omniscient
         public override string Species { get { return "AnalyzerStep"; } }
         public static readonly AnalyzerStepHookup[] Hookups = new AnalyzerStepHookup[]
         {
-            new TwoParameterAnalyzerStepHookup()
+            new TwoParameterAnalyzerStepHookup(),
+            new ChannelRangeStatisticAnalyzerStepHookup()
         };
-        public enum AnalyzerStepType { TWOPARAMETER }
+        public enum AnalyzerStepType { TWO_PARAMETER, CHANNEL_RANGES_TATISTIC }
         public AnalyzerStepType StepType { get; private set; }
         public Analyzer ParentAnalyzer { get; }
 
@@ -96,7 +97,7 @@ namespace Omniscient
         string param2Name;
         string outputParamName;
 
-        public TwoParameterAnalyzerStep(Analyzer analyzer, string name, uint id) : base(analyzer, name, id, AnalyzerStepType.TWOPARAMETER) 
+        public TwoParameterAnalyzerStep(Analyzer analyzer, string name, uint id) : base(analyzer, name, id, AnalyzerStepType.TWO_PARAMETER)
         {
             param1Name = "";
             param2Name = "";
@@ -121,10 +122,10 @@ namespace Omniscient
                     operation = "Ratio";
                     break;
             }
-            parameters.Add(new EnumParameter("Operation") { Value=operation, ValidValues= new List<string>() { "Sum", "Difference", "Product", "Ratio" } });
+            parameters.Add(new EnumParameter("Operation") { Value = operation, ValidValues = new List<string>() { "Sum", "Difference", "Product", "Ratio" } });
             parameters.Add(new StringParameter("Input Parameter 1", param1Name));
             parameters.Add(new StringParameter("Input Parameter 2", param2Name));
-            parameters.Add(new StringParameter("Output Parameter", param2Name));
+            parameters.Add(new StringParameter("Output Parameter", outputParamName));
             return parameters;
         }
         public override void ApplyParameters(List<Parameter> parameters)
@@ -254,4 +255,232 @@ namespace Omniscient
         }
     }
 
+    /// <summary>
+    /// Calculates a value from a range of channel data within the event
+    /// Operations include count, sum, average, max, min
+    /// </summary>
+    public class ChannelRangeStatisticAnalyzerStep : AnalyzerStep
+    {
+        public enum OperationType { Count, Sum, Average, Max, Min, StandardDeviation }
+        OperationType Operation { get; set; }
+        string channelParamName;
+        string outputParamName;
+
+        public ChannelRangeStatisticAnalyzerStep(Analyzer analyzer, string name, uint id) : base(analyzer, name, id, AnalyzerStepType.CHANNEL_RANGES_TATISTIC)
+        {
+            channelParamName = "";
+            outputParamName = "";
+        }
+
+        public override List<Parameter> GetParameters()
+        {
+            List<Parameter> parameters = new List<Parameter>();
+            string operation = "";
+            switch (Operation)
+            {
+                case OperationType.Count:
+                    operation = "Count";
+                    break;
+                case OperationType.Sum:
+                    operation = "Sum";
+                    break;
+                case OperationType.Average:
+                    operation = "Average";
+                    break;
+                case OperationType.Max:
+                    operation = "Max";
+                    break;
+                case OperationType.Min:
+                    operation = "Min";
+                    break;
+                case OperationType.StandardDeviation:
+                    operation = "StandardDeviation";
+                    break;
+            }
+            parameters.Add(new EnumParameter("Operation") { Value = operation, ValidValues = new List<string>() { "Sum", "Difference", "Product", "Ratio" } });
+            parameters.Add(new StringParameter("Channel Parameter", channelParamName));
+            parameters.Add(new StringParameter("Output Parameter", outputParamName));
+            return parameters;
+        }
+
+        public override void ApplyParameters(List<Parameter> parameters)
+        {
+            foreach (Parameter param in parameters)
+            {
+                switch (param.Name)
+                {
+                    case "Channel Parameter":
+                        channelParamName = param.Value;
+                        break;
+                    case "Output Parameter":
+                        outputParamName = param.Value;
+                        break;
+                    case "Operation":
+                        switch (param.Value)
+                        {
+                            case "Count":
+                                Operation = OperationType.Count;
+                                break;
+                            case "Sum":
+                                Operation = OperationType.Sum;
+                                break;
+                            case "Average":
+                                Operation = OperationType.Average;
+                                break;
+                            case "Max":
+                                Operation = OperationType.Max;
+                                break;
+                            case "Min":
+                                Operation = OperationType.Min;
+                                break;
+                            case "Standard Deviation":
+                                Operation = OperationType.StandardDeviation;
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+
+        private int Count(Event eve, List<DateTime> times, int startIndex)
+        {
+            int count = 0;
+            for (int i = startIndex; i < times.Count; i++)
+            {
+                if (times[i] > eve.EndTime) break;
+                count++;
+            }
+            return count;
+        }
+        private double Sum(Event eve, List<DateTime> times, List<double> vals, int startIndex)
+        {
+            double sum = 0;
+            for (int i = startIndex; i < times.Count; i++)
+            {
+                if (times[i] > eve.EndTime) break;
+                sum += vals[i];
+            }
+            return sum;
+        }
+
+        private double Max(Event eve, List<DateTime> times, List<double> vals, int startIndex)
+        {
+            double max = double.MinValue;
+            for (int i = startIndex; i < times.Count; i++)
+            {
+                if (times[i] > eve.EndTime) break;
+                if (vals[i] > max) max = vals[i];
+            }
+            return max;
+        }
+        private double Min(Event eve, List<DateTime> times, List<double> vals, int startIndex)
+        {
+            double min = double.MaxValue;
+            for (int i = startIndex; i < times.Count; i++)
+            {
+                if (times[i] > eve.EndTime) break;
+                if (vals[i] < min) min = vals[i];
+            }
+            return min;
+        }
+        private double StandardDeviation(Event eve, List<DateTime> times, List<double> vals, int startIndex)
+        {
+            int count = Count(eve, times, startIndex);
+            double average = Sum(eve, times, vals, startIndex) / count;
+            double sumSq = 0;
+            for (int i = startIndex; i < times.Count; i++)
+            {
+                if (times[i] > eve.EndTime) break;
+                sumSq += (vals[i] - average) * (vals[i] - average);
+            }
+
+            return Math.Sqrt(sumSq/count);
+        }
+
+        public override ReturnCode Run(Event eve, Dictionary<string, AnalyzerParameter> analysisParams)
+        {
+            // Validate parameters
+            SystemChannelParameter channelParam;
+            Parameter outputParam;
+            try
+            {
+                channelParam = analysisParams[channelParamName].Parameter as SystemChannelParameter;
+                outputParam = analysisParams[outputParamName].Parameter;
+            }
+            catch (Exception ex) { return ReturnCode.BAD_INPUT; }
+            if (outputParam.Type != ParameterType.Int && outputParam.Type != ParameterType.Double) return ReturnCode.BAD_INPUT;
+
+            // Load channel and get the times/vals/durations
+            Channel channel = channelParam.ToChannel();
+            channel.GetInstrument().LoadData(ChannelCompartment.Process, eve.StartTime, eve.EndTime);
+            List<DateTime> times = channel.GetTimeStamps(ChannelCompartment.Process);
+            List<double> vals = channel.GetValues(ChannelCompartment.Process);
+            List<TimeSpan> durations = null;
+            if (channel.GetChannelType() == Channel.ChannelType.DURATION_VALUE)
+                durations = channel.GetDurations(ChannelCompartment.Process);
+
+            // Fast forward to start time
+            int startIndex = 0;
+            while (startIndex < times.Count && times[startIndex] <= eve.StartTime) startIndex++;
+
+            int count;
+            double sum, stat;
+            switch (Operation)
+            {
+                case OperationType.Count:
+                    count = Count(eve, times, startIndex);
+                    outputParam.Value = count.ToString();
+                    break;
+                case OperationType.Sum:
+                    sum = Sum(eve, times, vals, startIndex);
+                    if (outputParam.Type == ParameterType.Int) outputParam.Value = ((int)sum).ToString();
+                    else outputParam.Value = sum.ToString();
+                    break;
+                case OperationType.Average:
+                    count = Count(eve, times, startIndex);
+                    sum = Sum(eve, times, vals, startIndex);
+                    double average = sum / count;
+                    if (outputParam.Type == ParameterType.Int) outputParam.Value = ((int)average).ToString();
+                    else outputParam.Value = average.ToString();
+                    break;
+                case OperationType.Max:
+                    stat = Max(eve, times, vals, startIndex);
+                    if (outputParam.Type == ParameterType.Int) outputParam.Value = ((int)stat).ToString();
+                    else outputParam.Value = stat.ToString();
+                    break;
+                case OperationType.Min:
+                    stat = Min(eve, times, vals, startIndex);
+                    if (outputParam.Type == ParameterType.Int) outputParam.Value = ((int)stat).ToString();
+                    else outputParam.Value = stat.ToString();
+                    break;
+                case OperationType.StandardDeviation:
+                    stat = StandardDeviation(eve, times, vals, startIndex);
+                    if (outputParam.Type == ParameterType.Int) outputParam.Value = ((int)stat).ToString();
+                    else outputParam.Value = stat.ToString();
+                    break;
+
+            }
+            return ReturnCode.SUCCESS;
+        }
+    }
+    public class ChannelRangeStatisticAnalyzerStepHookup : AnalyzerStepHookup
+    {
+        public ChannelRangeStatisticAnalyzerStepHookup()
+        {
+            TemplateParameters = new List<ParameterTemplate>()
+            {
+                new ParameterTemplate("Operation", ParameterType.Enum, new List<string>(){ "Count", "Sum", "Average", "Max", "Min", "Standard Deviation" }),
+                new ParameterTemplate("Channel Parameter", ParameterType.String),
+                new ParameterTemplate("Output Parameter", ParameterType.String)
+            };
+        }
+
+        public override string Type { get { return "Channel Range Statistic"; } }
+        public override AnalyzerStep FromParameters(Analyzer parent, string newName, List<Parameter> parameters, uint id)
+        {
+            ChannelRangeStatisticAnalyzerStep step = new ChannelRangeStatisticAnalyzerStep(parent, newName, id);
+            step.ApplyParameters(parameters);
+            return step;
+        }
+    }
 }
