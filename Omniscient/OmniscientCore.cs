80c4599cac4e5b50fa9dfc2c23d1379e8a591f98
Marc L. Ruch <ruch@lanl.gov>
2024-12-05 13:52:00 -0700

    Updated version (0.9.10)

--- Omniscient/OmniscientCore.cs
+++ Omniscient/OmniscientCore.cs
@@ -28,7 +28,7 @@ namespace Omniscient
         /// </summary>
         public static readonly string VERSION = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Major.ToString() + "." +
             System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Minor.ToString() + "." +
-            System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Build.ToString() + "b23-08-28";
+            System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Build.ToString();
 
         public event EventHandler ViewChanged;
         public event EventHandler<InstrumentEventArgs> InstrumentActivated;
